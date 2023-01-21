e588908c58e3e46a8c4669c66426283d2a871939
ozen-hub <info.ozenhub@gmail.com>
2023-01-21 11:04:35 +0530

    3

--- src/lk/ise/pos/dao/DataAccessCode.java
+++ src/lk/ise/pos/dao/DataAccessCode.java
@@ -2,10 +2,8 @@ package lk.ise.pos.dao;
 
 import lk.ise.pos.entity.Customer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataAccessCode {
@@ -22,9 +20,62 @@ public class DataAccessCode {
         preparedStatement.setDouble(4,c.getSalary());
         return preparedStatement.executeUpdate()>0;
     }
-    /*public boolean updateCustomer(Customer c){}
-    public Customer findCustomer(String id){}
-    public boolean deleteCustomer(String id){}
-    public List<Customer> allCustomers(){}*/
+    public boolean updateCustomer(Customer c) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
+                "root","1234");
+        String sql="UPDATE customer SET name=?, address=?, salary=? WHERE id=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,c.getName());
+        preparedStatement.setString(2,c.getAddress());
+        preparedStatement.setDouble(3,c.getSalary());
+        preparedStatement.setString(4,c.getId());
+        return preparedStatement.executeUpdate()>0;
+    }
+    public Customer findCustomer(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
+                "root","1234");
+        String sql="SELECT * FROM customer WHERE id=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            return new Customer(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDouble(4)
+            );
+        }
+        return null;
+    }
+    public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
+                "root","1234");
+        String sql="DELETE FROM customer WHERE id=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,id);
+        return preparedStatement.executeUpdate()>0;
+    }
+    public List<Customer> allCustomers() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
+                "root","1234");
+        String sql="SELECT * FROM customer";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Customer> customerList= new ArrayList<>();
+        while (resultSet.next()){
+            customerList.add(new Customer(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDouble(4)
+            ));
+        }
+        return customerList;
+    }
     //=========Customer Manage Code============
 }
