39a28e78a262f639a0f65291d46585e4abb26c64
ozen-hub <info.ozenhub@gmail.com>
2023-01-21 11:18:14 +0530

    4

--- src/lk/ise/pos/dao/DataAccessCode.java
+++ src/lk/ise/pos/dao/DataAccessCode.java
@@ -1,5 +1,6 @@
 package lk.ise.pos.dao;
 
+import lk.ise.pos.db.DBConnection;
 import lk.ise.pos.entity.Customer;
 
 import java.sql.*;
@@ -9,11 +10,8 @@ import java.util.List;
 public class DataAccessCode {
     //=========Customer Manage Code============
     public boolean saveCustomer(Customer c) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
-                "root","1234");
         String sql="INSERT INTO customer VALUES(?,?,?,?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement(sql);
         preparedStatement.setString(1,c.getId());
         preparedStatement.setString(2,c.getName());
         preparedStatement.setString(3,c.getAddress());
@@ -21,11 +19,8 @@ public class DataAccessCode {
         return preparedStatement.executeUpdate()>0;
     }
     public boolean updateCustomer(Customer c) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
-                "root","1234");
         String sql="UPDATE customer SET name=?, address=?, salary=? WHERE id=?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement(sql);
         preparedStatement.setString(1,c.getName());
         preparedStatement.setString(2,c.getAddress());
         preparedStatement.setDouble(3,c.getSalary());
@@ -33,11 +28,8 @@ public class DataAccessCode {
         return preparedStatement.executeUpdate()>0;
     }
     public Customer findCustomer(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
-                "root","1234");
         String sql="SELECT * FROM customer WHERE id=?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement(sql);
         preparedStatement.setString(1,id);
         ResultSet resultSet = preparedStatement.executeQuery();
         if (resultSet.next()){
@@ -51,20 +43,14 @@ public class DataAccessCode {
         return null;
     }
     public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
-                "root","1234");
         String sql="DELETE FROM customer WHERE id=?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement(sql);
         preparedStatement.setString(1,id);
         return preparedStatement.executeUpdate()>0;
     }
     public List<Customer> allCustomers() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); // com.mysql.jdbc.Driver
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos",
-                "root","1234");
         String sql="SELECT * FROM customer";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement(sql);
         ResultSet resultSet = preparedStatement.executeQuery();
         List<Customer> customerList= new ArrayList<>();
         while (resultSet.next()){
